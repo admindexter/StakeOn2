b11544c81d1d4e5dd74bc6c1737c7c26985c3206
Dextersmart Harry <dextersmart@gmail.com>
2010-09-17 06:46:03 +0100

    Commit after Re-editing models

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,17 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :title, :location, :date_of_event, :photographer, :photo
 
+  has_attached_file :photo, :styles => { :thumbnail => "130x130>", :portrait => "150x150>",
+                    :snapshot => "200x200", :medium => "180x180>", :standard => "300x300>",
+                    :preview => "500x500>" },
+  					        :url => "/assets/articles/:id/:style/:basename.:extension",
+  					        :path => ":rails_root/public/assets/articles/:id/:style/:basename.:extension"
+
   validates_uniqueness_of :title
   validates_presence_of :title
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 500.kilobytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
 end
 
