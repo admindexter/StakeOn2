5822bda7de986601ca4fc42bd7549a78ccd495f7
Dextersmart Harry <dextersmart@gmail.com>
2010-09-17 06:43:16 +0100

    Commit after editing models

--- app/models/press.rb
+++ app/models/press.rb
@@ -3,17 +3,7 @@ class Press < ActiveRecord::Base
 
   acts_as_textiled :information
 
-  has_attached_file :photo, :styles => { :thumbnail => "100x100>", :portrait => "150x150>",
-                    :snapshot => "200x200", :medium => "180x180>", :standard => "300x300>",
-                    :preview => "500x500>" },
-  					        :url => "/assets/articles/:id/:style/:basename.:extension",
-  					        :path => ":rails_root/public/assets/articles/:id/:style/:basename.:extension"
-
   validates_uniqueness_of :title
   validates_presence_of :title, :information
-
-  validates_attachment_presence :photo
-  validates_attachment_size :photo, :less_than => 500.kilobytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,5 @@ class User < ActiveRecord::Base
 
   attr_accessible :username, :email, :password, :password_confirmation, :photo
   acts_as_authentic
-
-  has_attached_file :photo, :styles => { :thumbnail => "100x100>", :portrait => "150x150>",
-                    :snapshot => "200x200", :medium => "180x180>", :standard => "300x300>",
-                    :preview => "500x500>" },
-  					        :url => "/assets/articles/:id/:style/:basename.:extension",
-  					        :path => ":rails_root/public/assets/articles/:id/:style/:basename.:extension"
-
-  validates_attachment_presence :photo
-  validates_attachment_size :photo, :less_than => 500.kilobytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
 end
 
